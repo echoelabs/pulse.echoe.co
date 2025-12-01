647cf47108c574a57ef56c8c4dda78b406764f49
0xktn <0xktn@users.noreply.github.com>
2025-12-01 18:17:55 +0700

    fix: add cloudflare workers types for typecheck

--- src/worker.ts
+++ src/worker.ts
@@ -1,3 +1,5 @@
+/// <reference types="@cloudflare/workers-types" />
+
 export default {
   async fetch(request: Request, env: { ASSETS: Fetcher }): Promise<Response> {
     return env.ASSETS.fetch(request);
